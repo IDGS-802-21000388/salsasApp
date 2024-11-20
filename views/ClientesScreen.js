b935e253ddc70ccc4f0cb06da297d1e66ee37779
EdwinY-a <21000445@alumnos.utleon.edu.mx>
2024-11-20 11:57:00 -0600

    Nuevo método de Correo

--- views/ClientesScreen.js
+++ views/ClientesScreen.js
@@ -1,21 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-  ActivityIndicator,
-  StyleSheet,
-  Alert,
-  ScrollView,
-  TextInput,
+import {View, Text,TouchableOpacity, Image,ActivityIndicator, StyleSheet, Alert, ScrollView,TextInput,
 } from "react-native";
 import Modal from "react-native-modal";
 import { getUsers } from "../services/UsuarioService";
 import { getSaleDetailById } from "../services/DetalleVentaService";
 import { getProducts } from "../services/ProductService";
 import { getEncuestasByUserId } from "../services/EncuestaSatisfacionService";
-import { API_SERVICE_EMAIL } from '@env';
+import { API_BASE_PRUEBA } from '@env';
 
 function ClientesScreen() {
   const [usuarios, setUsuarios] = useState([]);
@@ -75,28 +66,31 @@ function ClientesScreen() {
     }
   };
 
-  const handleSendEmail = async (email, userId) => {
-    
+  const handleSendEmail = async (usuario) => {
     if (!selectedVenta || products.length === 0) {
       console.error("No hay datos disponibles para la compra.");
       return;
     }
   
     try {
-      const encuestasData = await getEncuestasByUserId(userId);
-      setEncuestas(encuestasData);  
-      const lastEncuesta = getLastEncuesta(encuestasData);      
+      const encuestasData = await getEncuestasByUserId(usuario.idUsuario);
+      const lastEncuesta = getLastEncuesta(encuestasData);
+  
+      if (!lastEncuesta) {
+        Alert.alert("Aviso", "El usuario no tiene encuestas disponibles.");
+        return;
+      }
+  
       const compra = {
         cantidad: selectedVenta.cantidad,
         total: selectedVenta.subtotal,
-        productos: products.map(product => ({
+        productos: products.map((product) => ({
           nombreProducto: product.nombreProducto,
-          fotografia: product.fotografia
+          fotografia: product.fotografia || null,
         })),
-        encuesta: lastEncuesta
       };
   
-      await sendEmail(email, compra);
+      await sendEmail(usuario.correo, compra, usuario, lastEncuesta);
     } catch (error) {
       console.error("Error al obtener encuestas", error);
       Alert.alert("Error", "No se pudo obtener las encuestas del cliente.");
@@ -111,48 +105,96 @@ function ClientesScreen() {
     return encuestas[0];
   };
   
-  const sendEmail = async (email, compra) => {
+  const sendEmail = async (email, compra, usuario, encuesta) => {
     if (!email) {
       Alert.alert("Error", "No se proporcionó un correo electrónico.");
       return;
     }
-
+  
     if (!compra || !compra.cantidad || !compra.total || !compra.productos) {
       Alert.alert("Error", "Información de la compra incompleta.");
       return;
     }
-
-    const templateParams = {
-      email,
-      cantidad: compra.cantidad,
-      total: compra.total,
-      productos: compra.productos,
-      procesoCompra: compra.encuesta.procesoCompra,
-      saborProducto: compra.encuesta.saborProducto,
-      recomendacion: compra.encuesta.recomendacion,
-      entregaProducto: compra.encuesta.entregaProducto,
-      presentacionProducto: compra.encuesta.presentacionProducto,
-      facilidadUsoPagina: compra.encuesta.facilidadUsoPagina,
+  
+    if (!encuesta) {
+      Alert.alert("Error", "No se encontró ninguna encuesta asociada.");
+      return;
+    }
+  
+    const LogoUrl = `https://drive.google.com/uc?export=view&id=1NjIiYJWBDYkn8DhVzyXuqciEETddfa8M`;
+  
+    const renderStars = (rating) => {
+      return "★".repeat(rating) + "☆".repeat(5 - rating);
     };
-
+  
+    const htmlMessage = `
+      <html>
+        <body style="font-family: Arial, sans-serif; line-height: 1.6; background-color: #f7f7f7; margin: 0; padding: 0;">
+          <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
+            <div style="text-align: center; margin-bottom: 20px;">
+              <img src="${LogoUrl}" alt="Salsas Reni" style="width: 150px;" />
+            </div>
+            <h1 style="color: #c31a23; text-align: center;">¡Gracias por tu compra, ${usuario.nombre}!</h1>
+            <p style="text-align: center; color: #333;">Estos son los detalles de tu pedido:</p>
+            <div style="background-color: #f9f9f9; margin-top: 20px; padding: 15px; border-radius: 8px;">
+              <p style="font-weight: bold; color: #333;">Cantidad de productos:</p>
+              <p style="color: #555;">${compra.cantidad}</p>
+              <p style="font-weight: bold; color: #333;">Total:</p>
+              <p style="color: #555;">$${compra.total.toFixed(2)}</p>
+            </div>
+            <h3 style="color: #c31a23; text-align: center; margin-top: 20px;">Productos adquiridos</h3>
+            <div style="margin-top: 20px;">
+              ${compra.productos
+                .map(
+                  (p) => `
+                  <div style="display: flex; align-items: center; margin-bottom: 10px; border-bottom: 1px solid #ddd; padding-bottom: 10px;">
+                    <p style="font-weight: bold; color: #333;">${p.nombreProducto}</p>
+                  </div>`
+                )
+                .join("")}
+            </div>
+            <h3 style="color: #c31a23; text-align: center; margin-top: 20px;">Encuesta de Satisfacción</h3>
+            <div style="background-color: #f9f9f9; padding: 15px; border-radius: 8px;">
+              <p><strong>Proceso de Compra:</strong> ${encuesta.procesoCompra} ${renderStars(encuesta.procesoCompra)}</p>
+              <p><strong>Sabor del Producto:</strong> ${encuesta.saborProducto} ${renderStars(encuesta.saborProducto)}</p>
+              <p><strong>Entrega del Producto:</strong> ${encuesta.entregaProducto} ${renderStars(encuesta.entregaProducto)}</p>
+              <p><strong>Presentación del Producto:</strong> ${encuesta.presentacionProducto} ${renderStars(encuesta.presentacionProducto)}</p>
+              <p><strong>Facilidad de Uso de la Página:</strong> ${encuesta.facilidadUsoPagina} ${renderStars(encuesta.facilidadUsoPagina)}</p>
+            </div>
+            <div style="text-align: center; margin-top: 20px;">
+              <p style="color: #333;">Si tienes alguna duda o necesitas más información, no dudes en contactarnos.</p>
+              <p style="color: #555;">¡Gracias por elegir Salsas Reni!</p>
+            </div>
+          </div>
+        </body>
+      </html>
+    `;
+  
+    const templateParams = {
+      emails: [email],
+      mensaje: htmlMessage,
+    };
+  
     try {
-      const response = await fetch(`${API_SERVICE_EMAIL}/send-email`, {
+      const response = await fetch(`${API_BASE_PRUEBA}/PromoPorTipo/enviar-promocion`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(templateParams),
       });
-
+  
       if (!response.ok) {
         throw new Error("Error al enviar el correo");
       }
-
+  
       Alert.alert("Correo enviado", "El correo se ha enviado correctamente.");
     } catch (error) {
+      console.error("Error al enviar el correo:", error);
       Alert.alert("Error", "Hubo un problema al enviar el correo.");
     }
   };
+  
 
   return (
     <ScrollView style={styles.container}>
@@ -172,7 +214,7 @@ function ClientesScreen() {
           <Text>Rol: {usuario?.rol || "No encontrado"}</Text>
           {detailsViewed[usuario.idUsuario] && (
             <TouchableOpacity
-              onPress={() => handleSendEmail(usuario?.correo, usuario.idUsuario)}
+              onPress={() => handleSendEmail(usuario)}
               style={styles.emailButton}
             >
               <Text style={styles.emailText}>📧 Enviar Email</Text>
